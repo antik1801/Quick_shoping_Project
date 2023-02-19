78cad1928fbbeb41a5e72cbe1eb58a2e2dcc7f27
antik1801 <antik.edu@gmail.com>
2023-02-19 17:31:53 +0600

    Table updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,19 @@
-console.log('Successfully connected!');
+function totalCalculator(perUnit, totalUnit){
+    return perUnit*totalUnit;
+}
 document.getElementById('card-1').addEventListener('click',function(event){
     const productName = document.getElementById('coffee').innerText;
     const productPrice = document.getElementById('price').innerText;
     const productQuantity = document.getElementById('quantity').innerText;
     const tableBody = document.getElementById('table-body');
     const tr = document.createElement('tr');
+    
     tr.innerHTML=`
     <td>${1}</td>
     <td>${productName}</td>
     <td>${productPrice}</td>
-    <td>${productQuantity}</td>    
+    <td>${productQuantity}</td> 
+    <td>${0}</td>
     `
     tableBody.appendChild(tr);
 })
